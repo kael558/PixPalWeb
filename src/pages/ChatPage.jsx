8bd540ccd23057fb7f3e77c701bf39f7326d0558
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2024-06-17 20:34:17 -0400

    revert test

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -23,11 +23,18 @@ function ChatPage() {
 
 	useEffect(() => {
 		// first time user is signed in anonymously
+<<<<<<< HEAD
 		(async() => {
 			if (!(await isAuthenticated()) && name === "") {
 				loginAnonymously();
 			}
 		})();
+=======
+		if (!isAuthenticated() && name === "") {
+			//
+			loginAnonymously();
+		}
+>>>>>>> parent of 6f745e2... sign in anon
 	}, []);
 
 	return (
